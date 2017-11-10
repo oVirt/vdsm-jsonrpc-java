d24fbe34d22565811693da0378a257463902e40a
Piotr Kliczewski <piotr.kliczewski@gmail.com>
2017-11-10 12:19:38 +0100

    Release host level lock faster
    We need to make sure that thread executing a command is notified faster
    so for 500+ hosts there are no issues.
    
    
    Change-Id: If1888acae0b16928e53e9064a65b1f2b6624ae9c
    Bug-Url: https://bugzilla.redhat.com/1500739
    Signed-off-by: Piotr Kliczewski <piotr.kliczewski@gmail.com>

--- client/src/main/java/org/ovirt/vdsm/jsonrpc/client/internal/ResponseTracker.java
+++ client/src/main/java/org/ovirt/vdsm/jsonrpc/client/internal/ResponseTracker.java
@@ -25,6 +25,7 @@ import org.ovirt.vdsm.jsonrpc.client.ClientConnectionException;
 import org.ovirt.vdsm.jsonrpc.client.JsonRpcRequest;
 import org.ovirt.vdsm.jsonrpc.client.JsonRpcResponse;
 import org.ovirt.vdsm.jsonrpc.client.RequestAlreadySentException;
+import org.ovirt.vdsm.jsonrpc.client.reactors.ReactorClient;
 import org.ovirt.vdsm.jsonrpc.client.utils.LockWrapper;
 import org.ovirt.vdsm.jsonrpc.client.utils.ResponseTracking;
 import org.ovirt.vdsm.jsonrpc.client.utils.retry.RetryContext;
@@ -80,9 +81,6 @@ public class ResponseTracker implements Runnable {
         JsonNode id = req.getId();
         List<JsonNode> nodes = new CopyOnWriteArrayList<>();
         try (LockWrapper wrapper = new LockWrapper(this.lock)) {
-            if (!tracking.getClient().isOpen()) {
-                return;
-            }
             this.map.put(id, tracking);
             this.queue.add(id);
             nodes.add(id);
@@ -136,7 +134,6 @@ public class ResponseTracker implements Runnable {
     private void handleFailure(ResponseTracking tracking, JsonNode id) {
         remove(tracking, id, buildFailedResponse(tracking.getRequest()));
         if (tracking.isResetConnection()) {
-            this.hostToId.remove(tracking.getClient().getClientId());
             tracking.getClient().disconnect("Vds timeout occured");
         }
     }
@@ -178,13 +175,15 @@ public class ResponseTracker implements Runnable {
         String code = (String) map.get("code");
         String message = (String) map.get("message");
         JsonRpcResponse errorResponse = buildErrorResponse(null, 5022, message);
-        String hostname = code.substring(0, code.indexOf(":"));
 
         try (LockWrapper wrapper = new LockWrapper(this.lock)) {
-            this.hostToId.keySet().stream().filter(key -> key.startsWith(hostname)).forEach(key -> {
-                removeNodes(this.hostToId.get(key), errorResponse);
-                this.hostToId.remove(key);
-            });
+            if (ReactorClient.CLIENT_CLOSED.equals(message)) {
+                removeNodes(this.hostToId.get(code), errorResponse);
+            } else {
+                String hostname = code.substring(0, code.indexOf(":"));
+                this.hostToId.keySet().stream().filter(key -> key.startsWith(hostname))
+                        .forEach(key -> removeNodes(this.hostToId.get(key), errorResponse));
+            }
         }
     }
 
