94a8c9811219f2a40818063dfa635ec8b20becca
Artur Socha <asocha@redhat.com>
2021-12-15 11:42:28 +0100

    Move to GitHub Actions
    This patch migrates the project from Ovirt Jenkins CI to GitHub Actions.

---
+++ .automation/build-srpm.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -xe
+
+# Directory, where build artifacts will be stored, should be passed as the 1st parameter
+ARTIFACTS_DIR=${1:-exported-artifacts}
+export ARTIFACTS_DIR
+
+# Prepare source archive
+[[ -d rpmbuild/SOURCES ]] || mkdir -p rpmbuild/SOURCES
+
+./autogen.sh --system
+./configure
+
+# clean
+rm -rf rpmbuild/SOURCES/*
+make clean
+
+# build tarballs
+make dist
+
+SUFFIX=".$(date -u +%Y%m%d%H%M%S).git$(git rev-parse --short HEAD)"
+
+# Build SRPMs
+rpmbuild \
+    -D "_topdir rpmbuild" \
+    -D "release_suffix ${SUFFIX}" \
+    -ts ./*.tar.gz
